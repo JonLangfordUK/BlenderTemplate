33f7ecebea6a4fa45ab07d3a66ea71ccfcacf845
Jon <jonlangforduk@gmail.com>
2025-03-13 00:01:25 +0000

    Adding error location when stating with blender

--- __init__.py
+++ __init__.py
@@ -1,18 +1,20 @@
 """ Basic example of a Blender Addon using a register and unregister function. """
 
-from src.example_class import Animal, Dog, Cat
+## ERROR - This is not working, the classes are not being imported
+#from src.example_class import Animal, Dog, Cat
 
 def register():
     """
     Example of a basic register function for a Blender Addon.
     """
-    animal_a: Animal = Dog()
-    animal_b: Animal = Cat()
+    #animal_a: Animal = Dog()
+    #animal_b: Animal = Cat()
 
-    print("Registering Blender Addon")
+    print(f'Registering {__name__.split(".")[-1]} addon')
+    
 
 def unregister():
     """
     Example of a basic unregister function for a Blender Addon.
     """
-    print("Unregistering Blender Addon")
+    print(f'Unregistering {__name__.split(".")[-1]} addon')
